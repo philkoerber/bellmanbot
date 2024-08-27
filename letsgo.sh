5c5e16731e68b1bb316ff7d17f9ee710a2374090
philkoerber <philipp.koerber.93@gmail.com>
2024-08-27 08:02:41 +0200

    update strt script

--- letsgo.sh
+++ letsgo.sh
@@ -3,7 +3,17 @@
 # Path to your client directory
 CLIENT_DIR="./client"
 
-echo "ok lets go! welcome to the bellmanBot!"
+echo "ok lets go! welcome to...!"
+
+echo "
+  _______ _          ____       _ _                       ____        _   
+ |__   __| |        |  _ \     | | |                     |  _ \      | |  
+    | |  | |__   ___| |_) | ___| | |_ __ ___   __ _ _ __ | |_) | ___ | |_ 
+    | |  | '_ \ / _ \  _ < / _ \ | | '_ ` _ \ / _` | '_ \|  _ < / _ \| __|
+    | |  | | | |  __/ |_) |  __/ | | | | | | | (_| | | | | |_) | (_) | |_ 
+    |_|  |_| |_|\___|____/ \___|_|_|_| |_| |_|\__,_|_| |_|____/ \___/ \__|"
+                                                                          
+                                                                          
 
 # Check if node_modules directory exists, indicating npm is initialized
 if [ ! -d "$CLIENT_DIR/node_modules" ]; then
